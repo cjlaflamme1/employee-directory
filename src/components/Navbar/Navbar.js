86d3c9003771817c79e9d738125baa461b1265b5
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-10-24 16:51:18 -0400

    navbar element created

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <h1>Navbar works!</h1>
+    )
+}
+
+export default Navbar;
